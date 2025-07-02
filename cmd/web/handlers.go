912d558fdf82d6ee35974b001b6e8e5c94fe3293
Olamiye Emmanuel <olamiyeoecpe2022@futa.edu.ng>
2025-07-02 16:32:56 +0100

    changes

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,33 +3,40 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+//Defined as a method against the application struct
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
 
-	ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+	files := []string{
+		"./ui/html/home.page.tmpl",
+		"./ui/html/base.layout.tmpl",
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
 
 	if err != nil {
-		log.Println(err.Error())
+		app.errorLog.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
+		return
 	}
 
 	err = ts.Execute(w, nil)
 	if err != nil {
-		log.Println(err.Error())
+		app.errorLog.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
 	}
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
-func showSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 
 	if err != nil || id < 1 {
@@ -40,7 +47,7 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Display a specific snippet with id: %v", id)
 }
 
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		w.Header().Set("Allow", "POST")
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,59 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog *log.Logger
+}
+
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
-
-	log.Println("Starting a new Server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-
+	f, err := os.OpenFile("/tmp/info.log", os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
 		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	infoLog := log.New(f, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile|log.LUTC)
+
+	app := &application {
+		errorLog: errorLog,
+		infoLog: infoLog,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet", app.showSnippet)
+	mux.HandleFunc("/snippet/create", app.createSnippet)
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+	
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	infoLog.Printf("Starting a new Server on %s", *addr)
+	
+	srv := &http.Server {
+		Addr: *addr,
+		ErrorLog: errorLog,
+		Handler: mux,
+	}
+	
+	infoLog.Printf("Starting server on %s", *addr)
+	
+	err = srv.ListenAndServe()
+	//err := http.ListenAndServe(*addr, mux)
+
+	if err != nil {
+		errorLog.Fatal(err)
 	}
 }
