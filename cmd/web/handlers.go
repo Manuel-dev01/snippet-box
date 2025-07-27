7a92e63a6fd7847c9afe81ee56c6b60d679828a0
Olamiye Emmanuel <olamiyeoecpe2022@futa.edu.ng>
2025-07-27 15:25:53 +0100

    changes

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,15 +5,16 @@ import (
 	"net/http"
 	"strconv"
 
+	"github.com/Manuel-dev01/snippet-box/pkg/forms"
 	"github.com/Manuel-dev01/snippet-box/pkg/models"
 )
 
 //Defined as a method against the application struct
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
+	// if r.URL.Path != "/" {
+	// 	app.notFound(w)
+	// 	return
+	// }
 
 	s, err := app.snippets.Latest()
 	if err != nil {
@@ -48,7 +49,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 
 	if err != nil || id < 1 {
 		app.notFound(w)
@@ -89,24 +90,36 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	//fmt.Fprintf(w, "Display a specific snippet with id: %v", id)
 }
 
+func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "create.page.tmpl", &templateData{
+		Form: forms.New(nil),
+	})
+}
+
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		w.Header().Set("Allow", "POST")
-		app.clientError(w, http.StatusMethodNotAllowed)
+	err := r.ParseForm()
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
 		return
 	}
 
-	title := "O	snail"
-	content := "O	snail\nClimb	Mount	Fuhji,	Slowly\n"
-	expires	:= "7"
+	form := forms.New(r.PostForm)
+	form.Required("title", "content", "expires")
+	form.MaxLength("title", 100)
+	form.PermittedValues("expires", "365", "7", "1")
 
-	id, err := app.snippets.Insert(title, content, expires)
-	if	err != nil {
+	if !form.Valid() {
+		app.render(w, r, "create.page.tmpl", &templateData{Form: form})
+		return
+	}
+
+	id, err := app.snippets.Insert(form.Get("title"), form.Get("content"), form.Get("expires"))
+	if err != nil {
 		app.serverError(w, err)
 		return
 	}
 
-	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
 
 	w.Write([]byte("Create a new snippet..."))
 }
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,13 +3,25 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
+
+	"github.com/Manuel-dev01/snippet-box/pkg/forms"
 	"github.com/Manuel-dev01/snippet-box/pkg/models"
 )
 
 type templateData struct {
 	CurrentYear int
+	Form *forms.Form
 	Snippet *models.Snippet
 	Snippets []*models.Snippet
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache(dir string) (map[string]*template.Template, error) {
@@ -27,7 +39,7 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 		name := filepath.Base(page)
 
 		// Parse the page template in to a template set.
-		ts, err := template.ParseFiles(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
